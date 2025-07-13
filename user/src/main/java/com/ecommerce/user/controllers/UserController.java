5b2b66444509e2715d6d22cd6eafef5d48f6cfbd
baliakM-dev <baliak.m@hotmail.com>
2025-07-13 19:39:27 +0200

    Observability
    Add grafana, loki, prometheus

--- user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -5,6 +5,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,23 +15,26 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+        log.info("Fetching user with id: {}", id);
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/api/users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@RequestBody UserRequest userRequest, @PathVariable String id) {
         boolean updated = userService.updateUser(id, userRequest);
         if (updated)
@@ -36,7 +42,7 @@ public class UserController {
         return   ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
         userService.addUser(userRequest);
         return ResponseEntity.ok("User added");
